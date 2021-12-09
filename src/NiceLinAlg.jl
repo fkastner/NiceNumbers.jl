f8c085c9f2ed1ff30cd40ff407f764b7aca79f14
Daniel Karrasch <Daniel.Karrasch@gmx.de>
2021-12-09 16:46:52 +0100

    compat changes for Julia v1.8

--- src/NiceLinAlg.jl
+++ src/NiceLinAlg.jl
@@ -1,10 +1,17 @@
 using LinearAlgebra: Hermitian, Symmetric, UpperTriangular, LowerTriangular
-using LinearAlgebra: _chol!, Cholesky, checkpositivedefinite
+using LinearAlgebra: _chol!, Cholesky, NoPivot, checkpositivedefinite
 import LinearAlgebra: cholesky!
 
-
-function cholesky!(A::Union{Hermitian{NiceNumber,S}, Symmetric{NiceNumber,S}} where S, ::Val{false}=Val(false); check::Bool = true)
-    C, info = _chol!(A.data, A.uplo == 'U' ? UpperTriangular : LowerTriangular)
-    check && checkpositivedefinite(info)
-    return Cholesky(C.data, A.uplo, info)
+@static if VERSION < v"1.8.0-DEV.1120"
+    function cholesky!(A::Union{Hermitian{NiceNumber}, Symmetric{NiceNumber}}, ::Val{false}=Val(false); check::Bool = true)
+        C, info = _chol!(A.data, A.uplo == 'U' ? UpperTriangular : LowerTriangular)
+        check && checkpositivedefinite(info)
+        return Cholesky(C.data, A.uplo, info)
+    end
+else
+    function cholesky!(A::Union{Hermitian{NiceNumber}, Symmetric{NiceNumber}}, ::NoPivot=NoPivot(); check::Bool = true)
+        C, info = _chol!(A.data, A.uplo == 'U' ? UpperTriangular : LowerTriangular)
+        check && checkpositivedefinite(info)
+        return Cholesky(C.data, A.uplo, info)
+    end
 end
